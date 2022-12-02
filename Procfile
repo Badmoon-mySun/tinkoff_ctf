e4c328b4dead1c2bc6c32bf7e86b659e00ada08c
an.r.khasanov <an.r.khasanov@tinkoff.ru>
2022-12-03 00:03:38 +0300

    Initial

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wsdi:app
---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, send_from_directory, redirect, request
+
+app = Flask(__name__)
+
+USER = "asd"
+FLAG = True
+SITE_URL = "http://localhost:5000"
+
+
+@app.route("/image")
+def fake_image():
+    global SITE_URL, USER, FLAG
+
+    if FLAG:
+        filename = "cat.jpeg"
+        return send_from_directory('static', filename)
+    
+    link = f"{SITE_URL}/admin/edit?username={USER}&is_admin=on"
+    
+    print("redirect to ", link)
+    
+    return redirect(link, 302)
+
+
+@app.route("/setting")
+def setting():
+    global USER, FLAG
+
+    username = request.values.get("username")
+
+    USER = username if username else USER
+    FLAG = True if request.values.get("flag") == 'on' else False
+
+    return f"username={USER}, flag={FLAG}"
+    
